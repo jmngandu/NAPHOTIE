ac3bc6b007d51201711e79b2ad02899efb189987
jmngandu <jamesmngandu@gmail.com>
2024-04-26 22:48:04 +0300

    added home page

--- naphotie/src/Components/Header.jsx
+++ naphotie/src/Components/Header.jsx
@@ -1,7 +1,20 @@
 export const Header = () => {
+  // const time = new Date();
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
   return (
     <>
       <div className="header-sect">
+        <small>Time: {timeOfDay}</small>
         <h1>NAPHOTIE</h1>
         <nav className="nav-sect">
           <p>PLANTS</p>
