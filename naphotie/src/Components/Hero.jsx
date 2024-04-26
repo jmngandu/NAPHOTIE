cf423a7cce59c17deb9f3add6c768a9963a6921f
jmngandu <jamesmngandu@gmail.com>
2024-04-26 15:32:18 +0300

    added home page

--- naphotie/src/Components/Hero.jsx
+++ naphotie/src/Components/Hero.jsx
@@ -4,7 +4,8 @@ export const Hero = (props) => {
       <div className="hero-sect">
         <div className="page-file">
           <img src="/src/assets/Plants/scaled.jpeg" />
-          <p>an image of a plant</p>
+          <p>A tea plant on the kenyan highlands</p>
+          <p>location kenya</p>
         </div>
       </div>
     </>
