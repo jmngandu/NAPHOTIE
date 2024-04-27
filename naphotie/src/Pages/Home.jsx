19d2c1f4e6dccac17f272fea8a6afef6c3328f13
jmngandu <jamesmngandu@gmail.com>
2024-04-27 04:09:34 +0300

    added home page

--- naphotie/src/Pages/Home.jsx
+++ naphotie/src/Pages/Home.jsx
@@ -5,16 +5,7 @@ import PlantsData from "../Components/PlantsData";
 
 export const Home = () => {
   const PlantDataset = PlantsData.map((Plants) => {
-    return (
-      <Hero
-        key={Plants.id}
-        Plants={Plants}
-        // image={Plants.image}
-        // name={Plants.name}
-        // location={Plants.location}
-        // openSpots={Plants.openSpots}
-      />
-    );
+    return <Hero key={Plants.id} Plants={Plants} />;
   });
   return (
     <>
