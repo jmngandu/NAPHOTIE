4e95cd6b162802413172cb074cb7d12a0cd45b0d
jmngandu <jamesmngandu@gmail.com>
2024-04-27 03:32:34 +0300

    added home page

--- naphotie/src/Components/Header.jsx
+++ naphotie/src/Components/Header.jsx
@@ -14,13 +14,13 @@ export const Header = () => {
   return (
     <>
       <div className="header-sect">
-        <small>Time: {timeOfDay}</small>
         <h1>NAPHOTIE</h1>
         <nav className="nav-sect">
           <p>PLANTS</p>
           <p>ANIMALS</p>
           <p>LAND</p>
         </nav>
+        <small>Time: {timeOfDay}</small>
       </div>
     </>
   );
--- naphotie/src/Pages/Home.jsx
+++ naphotie/src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "../Components/Footer";
 import { Header } from "../Components/Header";
 import { Hero } from "../Components/Hero";
 import PlantsData from "../Components/PlantsData";
@@ -6,6 +7,7 @@ export const Home = () => {
   const PlantDataset = PlantsData.map((Plants) => {
     return (
       <Hero
+        key={Plants.id}
         image={Plants.image}
         name={Plants.name}
         location={Plants.location}
@@ -16,6 +18,7 @@ export const Home = () => {
     <>
       <Header />
       <div className="Hero-page">{PlantDataset}</div>
+      <Footer />
     </>
   );
 };
