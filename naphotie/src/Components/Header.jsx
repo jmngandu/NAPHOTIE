a0dcaf4c9b2dc0efd0f27a0ef21d93f59e06b51c
jmngandu <jamesmngandu@gmail.com>
2024-04-29 15:39:23 +0300

    commit

--- naphotie/src/Components/Header.jsx
+++ naphotie/src/Components/Header.jsx
@@ -1,27 +1,27 @@
-// export const Header = () => {
-//   // const time = new Date();
-//   const date = new Date();
-//   const hours = date.getHours();
-//   let timeOfDay;
+export const Header = () => {
+  // const time = new Date();
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-//   if (hours < 12) {
-//     timeOfDay = "Morning";
-//   } else if (hours >= 12 && hours < 17) {
-//     timeOfDay = "Afternoon";
-//   } else {
-//     timeOfDay = "Night";
-//   }
-//   return (
-//     <>
-//       <div className="header-sect">
-//         <h1>NAPHOTIE</h1>
-//         <nav className="nav-sect">
-//           <p>PLANTS</p>
-//           <p>ANIMALS</p>
-//           <p>LAND</p>
-//         </nav>
-//         <small>Time: {timeOfDay}</small>
-//       </div>
-//     </>
-//   );
-// };
+  if (hours < 12) {
+    timeOfDay = "Morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "Afternoon";
+  } else {
+    timeOfDay = "Night";
+  }
+  return (
+    <>
+      <div className="header-sect">
+        <h1>NAPHOTIE</h1>
+        <nav className="nav-sect">
+          <p>PLANTS</p>
+          <p>ANIMALS</p>
+          <p>LAND</p>
+        </nav>
+        <small>Time: {timeOfDay}</small>
+      </div>
+    </>
+  );
+};
