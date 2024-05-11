f3a51959153395edc99952d66b116e78b5a1cdf5
jmngandu <jamesmngandu@gmail.com>
2024-05-11 23:07:49 +0300

    added location

--- naphotie/src/Components/Footer.jsx
+++ naphotie/src/Components/Footer.jsx
@@ -1,22 +1,9 @@
+import { FooterDetails } from "./FooterDetails";
+
 export const Footer = () => {
   return (
     <>
-      <div className="footer-sect">
-        <h1>NAPHOTIE</h1>
-        <span>JMNGANDU FOUNDATION INCORPORATION WITH JTHREEGLOBALLY</span>
-        <div className="foot-more">
-          <div>FAQS</div>
-          <div>Contacts</div>
-          <div className="location-page">
-            <div className="location-title">Location</div>
-            <div className="location-map">
-              <h1>hello world</h1>
-            </div>
-          </div>
-          <div>About</div>
-        </div>
-        <small id="conc">2024 all rights reserved</small>
-      </div>
+      <FooterDetails />
     </>
   );
 };
---
+++ naphotie/src/Components/FooterDetails.jsx
@@ -0,0 +1,22 @@
+export const FooterDetails = () => {
+  return (
+    <>
+      <div className="footer-sect">
+        <h1>NAPHOTIE</h1>
+        <span>JMNGANDU FOUNDATION INCORPORATION WITH JTHREEGLOBALLY</span>
+        <div className="foot-more">
+          <div>FAQS</div>
+          <div>Contacts</div>
+          <div className="location-page">
+            <div className="location-title">Location</div>
+            <div className="location-map">
+              <h1>hello world</h1>
+            </div>
+          </div>
+          <div>About</div>
+        </div>
+        <small id="conc">2024 all rights reserved</small>
+      </div>
+    </>
+  );
+};
