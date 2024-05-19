bbbbddd8e1d232cf1b80045fa03e57dbf4980ad8
jmngandu <jamesmngandu@gmail.com>
2024-05-19 08:39:39 +0300

    naphotie

--- naphotie/src/Components/FooterDetails.jsx
+++ naphotie/src/Components/FooterDetails.jsx
@@ -18,7 +18,6 @@ export const FooterDetails = () => {
             <div className="location-title">Location</div>
             <div className="location-map">{ourLocation()}</div>
           </div>
-          <p id="about-text">Naphotie is all about nature</p>
           <div id="about-page">About</div>
         </div>
         <small id="conc">2024 all rights reserved</small>
