5de07ace053497e7d2d1fb49230920e5f0ff0bb1
jmngandu <jamesmngandu@gmail.com>
2024-07-22 16:36:25 +0300

    naphotie

--- naphotie/src/Components/Login.jsx
+++ naphotie/src/Components/Login.jsx
@@ -1,21 +1,27 @@
 import { Form, Link } from "react-router-dom";
+import { useState } from "react";
 import VerifyHuman from "./VerifyHuman";
+
 export default function Login() {
   return (
     <>
       <div className="login-page">
         <h3>Sign in to NAPHOTIE</h3>
         <form>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+
+          <VerifyHuman />
+
+          <button type="submit">
+            <Link to="/Home">Sign in</Link>
+          </button>
+          <p>
+            <Link>create account</Link>
+          </p>
         </form>
-        <VerifyHuman />
-        <button type="submit">
-          <Link to="/Home">Sign in</Link>
-        </button>
-        <p>
-          <Link>create account</Link>
-        </p>
       </div>
     </>
   );
--- naphotie/src/Components/VerifyHuman.jsx
+++ naphotie/src/Components/VerifyHuman.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
 const VerifyHuman = () => {
   return (
     <div>
       <h1>Verify you're human</h1>
       <p>Please complete the captcha or verification process.</p>
-      <input type="checkbox" />
+      <label>
+        <input type="checkbox" />I agree to the terms and conditions
+      </label>
     </div>
   );
 };
