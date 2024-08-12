c74456aab1a5c73134d57e542c86d1770fe1c70c
jmngandu <jamesmngandu@gmail.com>
2024-08-12 13:54:41 +0300

    naphotie

--- naphotie/src/Components/Hero.jsx
+++ naphotie/src/Components/Hero.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const Hero = (props) => {
   let badgeInfo;
   if (props.Plants.openSpots === 0) {
@@ -10,7 +12,7 @@ export const Hero = (props) => {
       <div className="hero-sect">
         <div className="page-file">
           {badgeInfo && <div className="hero-badge">{badgeInfo}</div>}
-          <img src={`/src/assets/Plants/${props.Plants.image}`} />
+          <img src={`src/images/Plants/${props.Plants.image}`} />
           <h3>{props.Plants.name}</h3>
           <p>{props.Plants.location}</p>
           <p>Quantity : {props.Plants.Quantity}</p>
