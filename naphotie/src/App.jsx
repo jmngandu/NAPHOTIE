c59455a6cb41dfc146e7d0e54246319afc074360
jmngandu <jamesmngandu@gmail.com>
2024-10-24 17:35:47 +0300

    naphotie

--- naphotie/src/App.jsx
+++ naphotie/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="" element={<Login />} />
         <Route path="Signup" element={<SignUp />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
