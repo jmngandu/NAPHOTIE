6a81b34eafbf97867587a45b846e09082cdf720c
jmngandu <jamesmngandu@gmail.com>
2024-11-22 22:39:54 +0300

    naphotie

---
+++ naphotie/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwVCE4iKF-qboQgXR5BP2l0weqWMmmJFA",
+  authDomain: "naphotie.firebaseapp.com",
+  projectId: "naphotie",
+  storageBucket: "naphotie.firebasestorage.app",
+  messagingSenderId: "177394085962",
+  appId: "1:177394085962:web:d81706aac35b58bb4af6b3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
